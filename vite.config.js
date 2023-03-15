4d4c43413ff2baa3709ca017ad783033a5b637bb
JulianaBMattos <julianabarcellosmattos@gmail.com>
2023-03-15 18:01:24 -0300

    added deploy base

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+  plugins: [vue()],
   define: {     // https://vue-i18n.intlify.dev/guide/advanced/optimization.html#reduce-bundle-size-with-feature-build-flags
     __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: true,
     __INTLIFY_PROD_DEVTOOLS__: false,
   },
-  plugins: [vue()],
   server: {
     host: '0.0.0.0'
   },
